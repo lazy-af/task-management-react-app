7164b1ef2b839059927dece89ec2fba38c0fe8ba
Shivam <shivamgoyal2822@gmail.com>
2020-08-10 13:03:17 +0530

    Updates

---
+++ precache-manifest.e71a33e917af8aa1e55a1d90292252f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a4759bd9a7fffaaaab8ec607a5deff",
+    "url": "/task-management-react-app/index.html"
+  },
+  {
+    "revision": "4be44343f9845cc119ec",
+    "url": "/task-management-react-app/static/css/main.2bd9caae.chunk.css"
+  },
+  {
+    "revision": "449d59638a24ad298831",
+    "url": "/task-management-react-app/static/js/2.ec718e66.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-management-react-app/static/js/2.ec718e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be44343f9845cc119ec",
+    "url": "/task-management-react-app/static/js/main.90bd3b21.chunk.js"
+  },
+  {
+    "revision": "7e06001efd9ad034e732",
+    "url": "/task-management-react-app/static/js/runtime-main.e5a259a9.js"
+  }
+]);
